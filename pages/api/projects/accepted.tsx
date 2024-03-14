294d8377031e91c01669b41d64383b0689377273
aidenwong812 <aidenwong812@users.noreply.github.com>
2024-03-14 11:51:39 +0000

    Apply formatting changes

--- pages/api/projects/accepted.tsx
+++ pages/api/projects/accepted.tsx
@@ -14,7 +14,7 @@ class sendAcceptedProject {
       tracks: request.tracks
         .map(
           (one) =>
-            `<li key=${one.name} style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;Margin-bottom:15px;margin-left:0;color:#8c8c8c;font-size:14px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#d2d2d2;font-size:14px">${one.name}</p></li>`
+            `<li key=${one.name} style="-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;Margin-bottom:15px;margin-left:0;color:#8c8c8c;font-size:14px"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#d2d2d2;font-size:14px">${one.name}</p></li>`,
         )
         .join(""),
       numberOfTracks: request.tracks.length,
